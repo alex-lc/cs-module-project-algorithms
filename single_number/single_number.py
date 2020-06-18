8d6aa822524ae2f8f272c109ca169c9f5a490ecd
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-06-18 15:42:08 -0500

    single_number refactored

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,34 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+# Day 1:
+# def single_number(arr):
+#     # loop through array
+#     # if the count of the current item is only equal to 1,
+#     # then return that individual value
+#     for i in arr:  # O(n)
+#         if arr.count(i) == 1:  # O(n)
+#             return i
+
+# Day 2:
+def single_number(arr):
+
+    # create a dictionary to hold our duplicate counts
+    dupes = {}
+
+    for num in arr:
+        # check if num is in our dictionary
+        if num in dupes:
+            # delete it from the dictionary
+            del dupes[num]
+        else:
+            # add it to the dictionary and set its value to 1
+            dupes[num] = 1
+
+    for key, val in dupes.items():
+        if val == 1:
+            return key
 
 
 if __name__ == '__main__':
